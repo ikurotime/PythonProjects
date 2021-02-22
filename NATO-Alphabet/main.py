12d3a84a23a26dd25a9a38144b098be3a76f0bf7
OrtoalresaDev <developer@ortoalresa.com>
2021-02-22 11:21:25 +0100

    Finished PasswordManager

--- NATO-Alphabet/main.py
+++ NATO-Alphabet/main.py
@@ -1,11 +1,17 @@
 import pandas
 
-
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-new_dict = {row.letter:row.code for index,row in data.iterrows()}
-print(new_dict)
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Put a word os something, you know\n").upper()
-user_nato = [new_dict[letter] for letter in user_input]
-print(user_nato)
+new_dict = {row.letter: row.code for index, row in data.iterrows()}
+#print(new_dict)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_input():
+    user_input = input("Put a word or something, you know\n").upper()
+    try:
+        user_nato = [new_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Only letters in the field, please")
+        generate_input()
+    else:
+        print(user_nato)
+generate_input()
