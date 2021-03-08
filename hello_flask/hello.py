d62e0ac75a01df8e50d2d01d7cc231c1fe85f24e
ikurotime <davidhuertas11@gmail.com>
2021-03-08 20:45:10 +0100

    hello in Flask

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,13 +1,30 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
+random_number = random.randint(0,9)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Higher or lower...</h1>' \
+           '<h3 style="text-align: center">Guess a number between 0 and 9</h3>' \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" width=500>'
 
-@app.route('/bye')
-def bye():
-    return 'Bye!'
+
+@app.route('/<int:number>')
+def guess(number):
+    if number > random_number:
+        return '<h1 style="text-align: center">Too high!!</h1>' \
+               '<p style="text-align: center">Try again!</p>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+    elif number < random_number:
+        return '<h1 style="text-align: center">Too low...</h1>' \
+               '<p style="text-align: center">Try again!</p>' \
+               '<img src= "https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
+    else:
+        return '<h1 style="text-align: center">You find me!</h1>' \
+               '<p style="text-align: center">Nice job!</p>' \
+               '<img src= "https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True)
